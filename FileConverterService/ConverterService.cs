6f8c283e98b2cf17bc5cfda0ed6118e8f14ccdf2
Steven Godin <steven.godin@honeywell.com>
2018-04-03 11:23:06 -0400

    Added Converter Service

---
+++ FileConverterService/ConverterService.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace FileConverterService
+{
+	public class ConverterService
+	{
+		private FileSystemWatcher _watcher;
+
+		public bool Start()
+		{
+			_watcher = new FileSystemWatcher(@"c:\temp\a", "*_in.txt")
+			{
+				IncludeSubdirectories = false,
+				EnableRaisingEvents = true
+			};
+
+			_watcher.Created += FileCreated;
+
+			return true;
+		}
+
+		private static void FileCreated(object sender, FileSystemEventArgs e)
+		{
+			var content = File.ReadAllText(e.FullPath);
+
+			var upperContent = content.ToUpperInvariant();
+			var dir = Path.GetDirectoryName(e.FullPath);
+			var convertedFileName = Path.GetFileName(e.FullPath) + ".converted.txt";
+			var convertedPath = Path.Combine(dir, convertedFileName);
+
+			File.WriteAllText(convertedPath, upperContent);
+		}
+
+		public bool Stop()
+		{
+			_watcher.Dispose();
+			return true;
+		}
+	}
+}
